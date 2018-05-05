1455efe2186a5d19c1d2238e13c10e10789b4d79
William Mura <william.mura@nuance.com>
2018-05-05 09:45:48 -0400

    Adding camera's view

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
     View
 } from 'react-native';
 
+import CameraView from './views/Camera';
+
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -24,15 +26,7 @@ export default class App extends Component<Props> {
     render() {
         return (
             <View style={styles.container}>
-	            <Text style={styles.welcome}>
-	            	Welcome to Lapin Urbain!
-	            </Text>
-	            <Text style={styles.instructions}>
-	            	To get started, edit App.js
-	            </Text>
-	            <Text style={styles.instructions}>
-	            	{instructions}
-	            </Text>
+                <CameraView />
             </View>
         );
     }
@@ -41,18 +35,6 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+    }
 });
