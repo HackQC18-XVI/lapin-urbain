042b847b74b054b16273fc894096642d0fd287c6
Sultan Wehaibi <x21@me.com>
2018-05-06 09:47:38 -0400

    Fix issues with connection to the server

--- src/views/WasteDescription.js
+++ src/views/WasteDescription.js
@@ -14,12 +14,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFF',
         position: 'absolute',
         height: '90%',
-        width: '100%',
+        width: '96%',
         bottom: 0,
+        left:8,
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        borderLeftWidth: 2,
+        borderRightWidth: 2,
+        borderTopWidth: 2,
+        borderColor: '#808080'
     },
 
     // Spinner
@@ -51,7 +56,7 @@ const styles = StyleSheet.create({
     },
 
     description:{
-        fontSize: 20,
+        fontSize: 22,
         fontWeight: 'bold',
         color: '#808080',
         top: 20,
@@ -60,23 +65,24 @@ const styles = StyleSheet.create({
     categorie:{
         fontSize: 18,
         fontWeight: 'bold',
-        color: '#808080',
-        top: 50,
+        color: '#b2b2b2',
+        top: 22,
         left: 10
     },
     collecte:{
         fontSize: 18,
         fontWeight: 'bold',
-        color: '#808080',
-        top: 70,
+        color: '#bfbfbf',
+        top: 24,
         left: 10
     },
     message:{
         fontSize: 12,
         fontWeight: 'bold',
         color: '#808080',
-        top: 90,
-        left: 10
+        top: 40,
+        left: 10,
+        width: '90%'
     }
 });
 
@@ -115,8 +121,10 @@ export default class Description extends Component<Props> {
             borderRadius: 30,
             backgroundColor: color,
             position: 'absolute',
-            right: 20,
-            top: 20
+            right: 14,
+            top: 20,
+            borderColor: '#f2f2f2',
+            borderWidth: 1
         }
         return (
             <View style={styles.container}>
@@ -126,12 +134,14 @@ export default class Description extends Component<Props> {
                 {!ApiService.loading && item ?
                     <View style={styles.content}>
                         <View style={styles.contentText}>
-                            <Text style={styles.description}>{item['cible-description'].toUpperCase() || item['description'].toUpperCase()}</Text>
-                            <Text style={styles.categorie}>{item['categorie']}</Text>
+                            <Text style={styles.description}>{(item['cible-description'] || item['description']).toUpperCase()}</Text>
+                            <Text style={styles.categorie}>{item['categorie'].toUpperCase()}</Text>
                             <Text style={styles.collecte}>{item['nom-collecte']}</Text>
-                            <Text style={styles.message}>{item['location']['properties']['MESSAGE_FR']}</Text>
+                            {item['location'] && item['location']['properties'] ?
+                                <Text style={styles.message}>{item['location']['properties']['MESSAGE_FR']}</Text> : null
+                            }   
                             {item['instructions-speciales'] ?
-                               <Text>Instructions: {item['instructions-speciales']}</Text> : null
+                               <Text style={styles.instructions}>Instructions: {item['instructions-speciales']}</Text> : null
                             }
                             <View style={stylePoint} />
                         </View>
